15698c2f5033fbbe0227c10a1d08fcfbec4c7e40
Tyler Gary <tygary@gmail.com>
2020-04-20 11:12:37 -0700

    Adding First Version

---
+++ code.js
@@ -0,0 +1,24 @@
+// search
+document.querySelector("#searchButton").addEventListener("click", searchGithub);
+function searchGithub() {
+    const searchUserText = document.querySelector("#searchUser").value;
+
+    // Obtain a list of users from the Github API that match searchUserText
+    //  The final result will contain an array under the key 'items'
+    // Pass this array to `renderUserList`
+}
+
+function renderUserList(githubUsers) {
+    let html = "";
+    html += "<ul>";
+    for (let i = 0; i < githubUsers.length; i++) {
+        let githubUser = githubUsers[i];
+        html += "<li>";
+        html += `<strong>${githubUser.login}</strong>`;
+        html += `<a target="_blank" href="${githubUser.html_url}">`;
+        html += "</li>";
+    }
+    html += "</ul>";
+
+    document.querySelector("#resultsContainer").innerHTML = html;
+}
